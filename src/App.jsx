551805ce39063336202c23576ab1c6849fbeccb0
Will Robertson <williamdrobertson42@gmail.com>
2023-05-21 18:36:15 +1000

    feat!: add ReactRouter scaffold

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+
 function App() {
-    return <h1>App</h1>
+    return (
+        <>
+            <Routes>
+                <Route path="/">
+                    <Route path="/geo" />
+                    <Route path="/weather" />
+                    <Route path="/lunar" />
+                </Route>
+            </Routes>
+        </>
+    );
 }
 
-export default App
+export default App;
