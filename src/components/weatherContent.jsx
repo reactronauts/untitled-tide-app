f188d97e6d9a30d4f34a8f7a7656da2fe420af52
Jordan Aston <gcab022124@coderacademy.edu.au>
2023-05-22 00:04:01 +1000

    weather-content component built (statically)

--- src/components/weatherContent.jsx
+++ src/components/weatherContent.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import '../stylesheets/weather-content.css'
+import searchIcon from '../images/search.png';
+import humidityIcon from '../images/humidity.png';
+import windIcon from '../images/wind.png';
+import cloudyIcon from '../images/cloudy.png';
 
 const WeatherContent = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className='container'>
+      <div className='weather'>
+        <div className='search'>
+          <input type='text' placeholder='Enter City Name'/>
+          <button><img src={searchIcon} alt='search icon'/></button>
+        </div>
+        <div className='winfo'>
+          <img src={cloudyIcon} alt='search icon'/>
+          <h1>22°C</h1>
+          <h2>London</h2>
+        </div>
+        <div className='details'>
+          <div className='col'>
+            <img src={humidityIcon} alt='humidity icon'/>
+              <div className='humidity'>
+                <p>20%</p>
+                <p>Humidity</p>
+              </div>
+          </div>
+          <div className='col'>
+          <img src={windIcon} alt='wind icon'/>
+            <div className='wind'>
+              <p>2 km/h</p>
+              <p>Wind</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
