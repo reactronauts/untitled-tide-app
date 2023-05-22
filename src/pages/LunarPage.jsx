95118a8292e381975c5a94226aa6a2c5f8737675
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-05-22 12:14:07 +1000

    feat: LunarPage functional initialised

---
+++ src/pages/LunarPage.jsx
@@ -0,0 +1,11 @@
+import { GetUserLocation } from "../components/GetUserLocation";
+
+export function LunarPage(props) {
+    return (
+        <div>
+            <h1>Lunar Page</h1>
+                <GetUserLocation />
+        </div>
+    )
+}
+
