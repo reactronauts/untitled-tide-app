cfc03315ae9dcd8decf1152f48e74744377ebec7
Jordan Aston <gcab022124@coderacademy.edu.au>
2023-05-22 14:38:42 +1000

    inputs and top buttons components built statically

--- src/pages/weatherPage.jsx
+++ src/pages/weatherPage.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import WeatherContent from '../components/WeatherContent'; 
+import UilReact from '@iconscout/react-unicons/icons/uil-react';
+import TopButtons from '../components/TopButtons';
+import Inputs from '../components/Inputs';
 
 const WeatherPage = () => {
   return (
-    <>
-      <WeatherContent />
-    </>
+    <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
+      <TopButtons />
+      <Inputs />
+    </div>
   );
 };
 
