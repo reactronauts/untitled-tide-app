4c3666b3fcdd61231fdeaa0d91bf600e9df73bb2
Jordan Aston <gcab022124@coderacademy.edu.au>
2023-05-22 16:13:21 +1000

    TimeAndLocation component built statically

--- src/pages/weatherPage.jsx
+++ src/pages/weatherPage.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import UilReact from '@iconscout/react-unicons/icons/uil-react';
 import TopButtons from '../components/TopButtons';
 import Inputs from '../components/Inputs';
+import TimeAndLocation from '../components/TimeAndLocation';
+import TemperatureAndDetails from '../components/TemperatureAndDetails';
 
 const WeatherPage = () => {
   return (
-    <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
+    <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-custom-purple h-fit shadow-xl shadow-gray-400'>
       <TopButtons />
       <Inputs />
+
+      <TimeAndLocation />
+      <TemperatureAndDetails />
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-green': 'rgb(204,228,218)',
+        'custom-purple': 'rgb(146,179,243)'
+      }
+    },
   },
   plugins: [],
 };
