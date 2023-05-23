923c0dce3ea2a26aa6e57efc4fc161dd8f3258eb
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-05-23 17:13:11 +1000

    feat: component structure and styling begun/edited

--- src/pages/LunarPage.jsx
+++ src/pages/LunarPage.jsx
@@ -1,11 +1,13 @@
-import { GetUserLocation } from "../components/GetUserLocation";
+
+import { LunarPageContainer } from "../components/LunarPageContainer";
 
 export function LunarPage(props) {
     return (
-        <div className="bg-[url('src/images/nightSky.jpg')] h-screen bg-cover">
+        <div className="bg-[url('src/images/nightSky.jpg')] h-screen bg-cover flex wrap justify-center items-center flex-col">
 
             <h1 className="text-white">Lunar Page</h1>
-                <GetUserLocation />
+                <LunarPageContainer />
+                    
         </div>
     )
 }
