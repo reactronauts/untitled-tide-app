c62e146e12db211c50bc80235a6aafeb6cd001b7
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-05-23 14:19:04 +1000

    styling!: custom colors for weather page combined with existing (merge conflict resolved to illegal seperate 'colors' defaults

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,13 +12,10 @@ export default {
         'dm-db': '#0A121B',
         'dm-y': '#854500',
         'dm-o': '#7F3100',
-      },
-    
-      colors: {
         'custom-green': 'rgb(204,228,218)',
         'custom-purple': 'rgb(146,179,243)',
         'custom-black': 'rgb(10,13,16)'
-      }
+      },
     },
   },
   plugins: [],
