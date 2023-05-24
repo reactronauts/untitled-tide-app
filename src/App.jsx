6904b183b02822a8c2624e48e1d5c09842073460
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-05-24 16:32:47 +1000

    GeolocationConextComponent added to wrap all routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import WeatherPage from './pages/WeatherPage';
+import GeolocationContextComponent from "./components/GeolocationContextComponent";
 
 function App() {
     return (
         <>
-            <Routes>
-                <Route path="/">
-                    <Route path="tides" />
-                    <Route path="weather" element={<WeatherPage />}/>
-                    <Route path="lunar" />
-                </Route>
-            </Routes>
+            <GeolocationContextComponent>
+                <Routes>
+                    <Route path="/">
+                        <Route path="tides" />
+                        <Route path="weather" element={<WeatherPage />}/>
+                        <Route path="lunar" />
+                    </Route>
+                </Routes>
+            </GeolocationContextComponent>
         </>
     );
 }
