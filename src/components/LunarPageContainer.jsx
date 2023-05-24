3e4d00313c7a17913371abf4e2b21cd805ad8331
ConnieJacques <111848988+ConnieJacques@users.noreply.github.com>
2023-05-24 15:52:59 +1000

    styling: basic responsive design outline added

--- src/components/LunarPageContainer.jsx
+++ src/components/LunarPageContainer.jsx
@@ -1,8 +1,7 @@
 
 import { GetUserLocation } from "../components/GetUserLocation";
 import Inputs from "./Inputs";
-import { useState } from "react";
-import { VisiblePlanets } from "./VisiblePlanets";
+import { useState, useEffect } from "react";
 import sunIcon from '../images/lunarImages/sun.svg'
 import moonIcon from '../images/lunarImages/moon.svg'
 import mercuryIcon from '../images/lunarImages/mercury.svg'
@@ -13,11 +12,6 @@ import saturnIcon from '../images/lunarImages/saturn.svg'
 import uranusIcon from '../images/lunarImages/uranus.svg'
 import neptuneIcon from '../images/lunarImages/neptune.svg'
 
-// api: https://home.openweathermap.org/api_keys
-
-
-
-// GET https://api.visibleplanets.dev/v3?latitude=32&longitude=-98
 
 export function LunarPageContainer() {
     const [planets, setPlanet] = useState([]);
@@ -41,53 +35,33 @@ export function LunarPageContainer() {
             });
     }
 
+    useEffect(() => {
+        getPlanetaryPositions()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
-    const [rises, setRises] = useState()
-
-    function getAstronomicalData() {
-        const astronomicalApi = 'https://api.openweathermap.org/data/2.5/weather?lat=' + defaultLocationSydney.latitude + '&lon=' + defaultLocationSydney.longitude + '&appid={apikey}'
-
-        fetch(astronomicalApi)
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                console.log(data)
-                setRises(data)
-            })
-            .catch((error) => {
-                console.log("Fetch failed!" + error);
-                return null;
-            });
-        }
-
-
-    const [locationName, setLocationName] = useState("");
+    // const [locationName, setLocationName] = useState("");
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        // getPlanetaryPositions();
-        getAstronomicalData();
-
+        getPlanetaryPositions();
     };
 
-
-
     return (
-        <div className="h-5/6 w-5/6 rounded-2xl bg-yellow ">
+        <div className="h-5/6 w-5/6 rounded-2xl bg-light-blue text-dark-blue font-sans p-3 m-10">
             <GetUserLocation /> 
 
             <Inputs
                 handleSubmit={handleSubmit}
-                setLocationName={setLocationName}
+                // setLocationName={setLocationName}
             />
 
             <div>
-                <h2>Currently Visible in the Sky:</h2>
+                <h2 className="text-white text-2xl sm:text-3xl text-center drop-shadow-lg">Currently Visible in the Sky:</h2>
                 <ul>
                     {planets &&
                         planets.map((planet) => (
-                            <li key={planet.name} className="p-2 inline-block ms-2 mt-2 text-center">
+                            <li key={planet.name} className="p-2 inline-block ms-2 mt-2 text-center text-white">
                                 {planet.name == 'Sun' ? <img src={sunIcon} className="h-8 w-8 viewBox=0 0 24 24 mx-auto"/> : null}
                                 {planet.name == 'Moon' ? <img src={moonIcon} className="h-8 w-8 viewBox=0 0 24 24 mx-auto"/> : null}
                                 {planet.name == 'Mercury' ? <img src={mercuryIcon} className="h-8 w-8 viewBox=0 0 24 24 mx-auto"/> : null}
