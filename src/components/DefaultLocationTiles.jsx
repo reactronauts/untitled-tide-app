b17d3facdd9f3c97c9ad1f737c9a734591b62bd8
sarahhlandis <sarahlandis95@gmail.com>
2023-05-26 11:34:00 +1000

    feat: added pb/pt on weather tiles on homepage

--- src/components/DefaultLocationTiles.jsx
+++ src/components/DefaultLocationTiles.jsx
@@ -86,7 +86,7 @@ const WeatherPage = () => {
   return (
     <div className='mx-auto max-w-screen-xl mt-4 p-5 bg-dark-blue rounded-xl'>
       <Inputs handleSubmit={handleSubmit} />
-      <div className="grid grid-cols-4 gap-6">
+      <div className="grid grid-cols-4 gap-6 pt-4 pb-2">
         {data.map((cityData, index) => (
           <div key={index} className="p-4 rounded-lg bg-orange min-w-[250px]">
             <TimeAndLocation data={cityData} />
