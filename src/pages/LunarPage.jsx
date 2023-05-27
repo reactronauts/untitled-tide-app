860066b349f4c626c05226015ca45a0a9b10e1ee
Will Robertson <williamdrobertson42@gmail.com>
2023-05-27 16:37:05 +1000

    fix: move bg to public dir

--- src/pages/LunarPage.jsx
+++ src/pages/LunarPage.jsx
@@ -1,11 +1,9 @@
-
 import { LunarPageContainer } from "../components/LunarPageContainer";
 
 export function LunarPage(props) {
     return (
-        <div className="bg-[url('src/images/nightSky.jpg')] h-auto min-h-screen bg-cover flex wrap justify-center items-center flex-col">
-             <LunarPageContainer />        
+        <div className="wrap flex h-auto min-h-screen flex-col items-center justify-center bg-[url('/nightSky.jpg')] bg-cover">
+            <LunarPageContainer />
         </div>
-    )
+    );
 }
-
